0268aabc456dc7044c838da2f19f26dfd88949a9
Bruce Sharpe <bruce.sharpe@gmail.com>
2017-09-02 00:51:08 -0700

    Made Python 3 compatible (because it's 2017) and pylint-clean (because I'm anal)

--- link_aggregator.py
+++ link_aggregator.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Combine individual meetup paper lists into one file."""
+from __future__ import print_function
 import glob
+from io import open  # pylint: disable=redefined-builtin
 import time
 
 
@@ -9,6 +12,7 @@ _OTHER_PAPERS_HEADING = "## Other suggestions"
 
 
 def parse_markdown(markdown_content):
+    """parse_markdown"""
     accepted_match = markdown_content.find(_ACCEPTED_PAPER_HEADING) \
                      + len(_ACCEPTED_PAPER_HEADING)
     other_suggestions_match = markdown_content.find(_OTHER_PAPERS_HEADING)
@@ -21,21 +25,27 @@ def parse_markdown(markdown_content):
 
 
 def parse_files(files):
-    markdown_file = open("summary.md", "w")
+    """parse_files"""
+    markdown_file = open("summary.md", "w", encoding='utf8')
     output_content = '# Papers suggested to date\n\n'
     for ffile in files:
         try:
-            content = open(ffile, 'r').read()
+            content = open(ffile, 'r', encoding='utf8').read()
             accepted, suggested = parse_markdown(content)
             output_content += "# %s\n\n## Chosen Paper\n%s\n\n" \
                               "## Other Suggestions\n%s\n\n" \
                               % (ffile[:-3], accepted, suggested)
-        except Exception as e:
-            print "Caught an exception: %s " % e
+        except Exception as exc:  # pylint: disable=broad-except
+            print("Caught an exception: %s " % exc)
     markdown_file.write(output_content)
 
 
-if __name__ == '__main__':
+def main():
+    """Execute from command line."""
     files = [f for f in glob.glob("*.md") if f not in _EXCLUDE_FILES]
     files.sort(key=lambda x: -time.mktime(time.strptime(x, "%Y-%m-%d.md")))
     parse_files(files)
+
+
+if __name__ == '__main__':
+    main()
