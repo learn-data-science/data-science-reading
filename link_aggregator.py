012eea08215a3767fcacb2283537d71acc2d844e
Arnie0426 <arnabhy@gmail.com>
2017-09-02 11:40:29 -0700

    add archives to summary.md

--- link_aggregator.py
+++ link_aggregator.py
@@ -24,27 +24,35 @@ def parse_markdown(markdown_content):
     return accepted_paper.strip(), suggested_papers.strip()
 
 
-def parse_files(files):
+def parse_files(files, archive_file=None):
     """parse_files"""
-    markdown_file = open("summary.md", "w", encoding='utf8')
-    output_content = '# Papers suggested to date\n\n'
-    for ffile in files:
-        try:
-            content = open(ffile, 'r', encoding='utf8').read()
-            accepted, suggested = parse_markdown(content)
-            output_content += "# %s\n\n## Chosen Paper\n%s\n\n" \
-                              "## Other Suggestions\n%s\n\n" \
-                              % (ffile[:-3], accepted, suggested)
-        except Exception as exc:  # pylint: disable=broad-except
-            print("Caught an exception: %s " % exc)
-    markdown_file.write(output_content)
+    with open("summary.md", "w", encoding="utf8") as markdown_file:
+        output_content = "# Papers suggested to date\n\n"
+        for ffile in files:
+            try:
+                content = open(ffile, "r", encoding="utf8").read()
+                accepted, suggested = parse_markdown(content)
+                output_content += "# %s\n\n## Chosen Paper\n%s\n\n" \
+                                  "## Other Suggestions\n%s\n\n" \
+                                  % (ffile[:-3], accepted, suggested)
+            except Exception as exc:  # pylint: disable=broad-except
+                print("Caught an exception: %s " % exc)
+
+        if archive_file:
+            try:
+                archive_content = open(archive_file, "r", encoding="utf8").read()
+                output_content += archive_content
+            except IOError as exc:
+                print("Couldn't parse archive file: %s" % exc)
+        markdown_file.write(output_content)
 
 
 def main():
     """Execute from command line."""
     files = [f for f in glob.glob("*.md") if f not in _EXCLUDE_FILES]
+    # hack: sorts files by date in the title
     files.sort(key=lambda x: -time.mktime(time.strptime(x, "%Y-%m-%d.md")))
-    parse_files(files)
+    parse_files(files, archive_file="archives_2016.md")
 
 
 if __name__ == '__main__':
